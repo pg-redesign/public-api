aaa475ad99dd6cba07e0bd7961f75c7c7735338a
Vamp <25523682+the-vampiire@users.noreply.github.com>
2019-05-26 18:42:27 -0400

    models: Course static getAll and getUpcoming

--- src/db/models/course.js
+++ src/db/models/course.js
@@ -2,9 +2,25 @@ const { Model } = require("../connection");
 const schemas = require("../../schemas");
 const TimestampsBase = require("./timestamps-base");
 
+const DEFAULT_SORT = ["start_date", "desc"];
+
 class Course extends TimestampsBase {
   static get tableName() {
     return "courses";
+  }
+
+  static getAll() {
+    return this.query().orderBy(...DEFAULT_SORT);
+  }
+
+  static async getUpcoming() {
+    return (
+      this.query()
+        .orderBy(...DEFAULT_SORT)
+        // only return courses that are upcoming (beyond current date)
+        .where("start_date", ">", new Date())
+        .limit(2)
+    );
   }
 
   static get jsonSchema() {
@@ -31,4 +47,5 @@ class Course extends TimestampsBase {
   }
 }
 
+Course.DEFAULT_SORT = DEFAULT_SORT;
 module.exports = Course;
