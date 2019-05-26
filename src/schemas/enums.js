efa6f9924ed298c7d6fc581f3b89f2135c9e411a
Vamp <25523682+the-vampiire@users.noreply.github.com>
2019-05-26 17:00:10 -0400

    schemas: enums source of truth

---
+++ src/schemas/enums.js
@@ -0,0 +1,14 @@
+module.exports = {
+  PaymentTypes: {
+    check: "check",
+    credit: "credit",
+  },
+  FormTypes: {
+    registration: "registration",
+    stripePayment: "stripePayment",
+  },
+  LanguageTypes: {
+    english: "english",
+    portuguese: "portuguese",
+  },
+};
