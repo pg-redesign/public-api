acf928faa957a4d1942dbe0de3352b6bb9935bd1
Vamp <25523682+the-vampiire@users.noreply.github.com>
2019-05-25 20:17:16 -0400

    enums: add LanguageType and lowercase resolvers

---
+++ src/api/resolvers/enums.js
@@ -0,0 +1,14 @@
+module.exports = {
+  PaymentTypes: {
+    CHECK: "check",
+    CREDIT: "credit",
+  },
+  FormTypes: {
+    REGISTRATION: "registration",
+    STRIPE_PAYMENT: "stripe_payment",
+  },
+  LanguageTypes: {
+    ENGLISH: "english",
+    PORTUGUESE: "portuguese",
+  },
+};
--- src/api/resolvers/index.js
+++ src/api/resolvers/index.js
@@ -1,10 +1,12 @@
 const typeResolvers = require("./types");
+const enumResolvers = require("./enums");
 const queryResolvers = require("./queries");
 const scalarResolvers = require("./scalars");
 const mutationResolvers = require("./mutations");
 
 module.exports = {
   ...typeResolvers,
+  ...enumResolvers,
   ...queryResolvers,
   ...scalarResolvers,
   ...mutationResolvers,
