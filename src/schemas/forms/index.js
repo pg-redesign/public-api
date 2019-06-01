e8f9dcc80f4f7c516be2b34a6bbda36eb8e91331
Vamp <vampiirecodes@gmail.com>
2019-06-01 15:45:47 -0400

    schemas: stripePayment form schema

--- src/schemas/forms/index.js
+++ src/schemas/forms/index.js
@@ -2,10 +2,8 @@ const enums = require("../enums");
 const registration = require("./registration");
 const stripePayment = require("./stripePayment");
 
-const forms = {};
-
-// ensures naming conistency across codebase
-forms[enums.FormTypes.registration] = registration;
-forms[enums.FormTypes.stripePayment] = stripePayment;
-
-module.exports = forms;
+module.exports = {
+  // ensures naming conistency across codebase
+  [enums.FormTypes.registration]: registration,
+  [enums.FormTypes.stripePayment]: stripePayment,
+};
