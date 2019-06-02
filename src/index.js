b24101211516e05b829165a2f0cb9c101650e20c
Vamp <vampiirecodes@gmail.com>
2019-06-02 04:54:37 -0400

    server: add services to context, integrate apollo error converter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const { ApolloServer } = require("apollo-server");
 const logger = require("@vampiire/node-logger")();
+const { ApolloErrorConverter } = require("apollo-error-converter");
 
 const utils = require("./utils");
 const { models } = require("./db");
 const schemas = require("./schemas");
+const services = require("./services");
 const typeDefs = require("./api/type-defs");
 const resolvers = require("./api/resolvers");
 
@@ -11,15 +13,20 @@ const resolvers = require("./api/resolvers");
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  formatError: new ApolloErrorConverter({
+    logger: logger.error.bind(logger),
+  }),
   context: (options) => {
     const { req } = options;
     // TODO: log requests, logger.request() config in node-logger
+
     return {
       req,
       utils,
       logger,
       models,
       schemas,
+      services,
     };
   },
 });
