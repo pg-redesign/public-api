b03790d8bd1582b351a8017c37d257a26f6187c8
Vamp <vampiirecodes@gmail.com>
2019-06-13 19:19:33 -0400

    forms: add mailing list form type and validation schema

--- src/schemas/forms/index.js
+++ src/schemas/forms/index.js
@@ -1,9 +1,11 @@
 const enums = require("../enums");
+const mailingList = require("./mailingList");
 const registration = require("./registration");
 const stripePayment = require("./stripePayment");
 
 module.exports = {
   // ensures naming conistency across codebase
+  [enums.FormTypes.mailingList]: mailingList,
   [enums.FormTypes.registration]: registration,
   [enums.FormTypes.stripePayment]: stripePayment,
 };
