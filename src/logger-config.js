10540596a3534b68af1576e1682160df9ba86b8b
Vamp <vampiirecodes@gmail.com>
2019-07-06 09:06:17 -0400

    logger config: configure express request logging
    - add graphql level to core logger

---
+++ src/logger-config.js
@@ -0,0 +1,37 @@
+const buildLogger = require("@vampiire/node-logger");
+const expressLogger = require("express-winston").logger;
+
+const inDevelopment = process.env.NODE_ENV !== "production";
+
+const logger = buildLogger({
+  levels: {
+    error: 0,
+    warn: 1,
+    graphql: 2,
+    debug: 3,
+  },
+  colors: {
+    error: "bold red",
+    warn: "italic yellow",
+    graphql: "bold blue",
+    debug: "cyan",
+  },
+});
+
+const requestLogger = expressLogger({
+  level: "graphql",
+  winstonInstance: logger,
+  colorize: inDevelopment,
+  msg: "IP [{{req.ip}}], status {{res.statusCode}}, {{res.responseTime}}ms",
+  // skip the gql playground ping requests
+  skip: req => inDevelopment && req.body.operationName === "IntrospectionQuery",
+  dynamicMeta: req => ({
+    ip: req.ip,
+    body: req.body,
+  }),
+});
+
+module.exports = {
+  logger,
+  requestLogger,
+};
