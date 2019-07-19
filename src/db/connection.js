c7700cb08143d4209d0fb24e676e5ecdb5d5176a
Vamp <vampiirecodes@gmail.com>
2019-07-19 17:41:29 -0400

    DB: refactored connection to support AWS RDS credentials
    - supports local or deployed connection credentials

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,11 +1,24 @@
 const { Model } = require("objection");
-const connection = require("knex")({
+const knex = require("knex");
+
+const {
+  DB_HOST,
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+  RDS_DB_NAME,
+  RDS_HOSTNAME,
+  RDS_USERNAME,
+  RDS_PASSWORD,
+} = process.env;
+
+const connection = knex({
   client: "pg",
   connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
+    host: DB_HOST || RDS_HOSTNAME,
+    user: DB_USER || RDS_USERNAME,
+    database: DB_NAME || RDS_DB_NAME,
+    password: DB_PASSWORD || RDS_PASSWORD,
   },
 });
 
