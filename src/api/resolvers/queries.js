8c2ca8c49bead1019235986d09e2a15e63712ffc
Vamp <vampiirecodes@gmail.com>
2019-08-02 17:52:35 -0400

    Queries: getCoursesByLocation type def, resolver, tests

--- src/api/resolvers/queries.js
+++ src/api/resolvers/queries.js
@@ -18,5 +18,17 @@ module.exports = {
       const { CourseLocation } = context.models;
       return CourseLocation.getAll();
     },
+
+    getCoursesByLocation: async (_, args, context) => {
+      const { courseLocationId } = args;
+      const { CourseLocation } = context.models;
+
+      const courseLocation = await CourseLocation.getBy({
+        id: courseLocationId,
+      });
+
+      // empty list expected by schema if location is not found
+      return courseLocation ? courseLocation.getCourses() : [];
+    },
   },
 };
