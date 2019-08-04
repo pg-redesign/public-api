9702a22ec4530d42ec5ea029d90bba2ee12d22e8
Vamp <vampiirecodes@gmail.com>
2019-08-03 21:14:10 -0400

    Type:  student and payment Type field defs and resolvers
    - Course.students, .payments
    - Student.courses, .payments
    - Payment.course, .student

--- src/api/resolvers/types.js
+++ src/api/resolvers/types.js
@@ -1,30 +1,48 @@
-module.exports = {
-  Course: {
-    name: (course, args, context) => {
-      const { fullCourseNames } = context.utils.constants;
+const Course = {
+  name: (course, args, context) => {
+    const { fullCourseNames } = context.utils.constants;
 
-      return args.short ? course.name : fullCourseNames[course.name];
-    },
-
-    date: (course, args, { utils: { format } }) => {
-      const { start, end, language } = args;
-      const { startDate, endDate } = course;
-
-      // full date range in language format if neither start nor end are passed
-      if ([start, end].every(arg => arg === undefined)) {
-        return format.courseDateRange(startDate, endDate, language);
-      }
-
-      // if there are args give precedence to start
-      return start ? startDate.toUTCString() : endDate.toUTCString();
-    },
-
-    description: (course, _, context) => {
-      // TODO: reimplement as a db fetch for admin editing
-      const { courseDescriptions } = context.utils.constants;
-      return courseDescriptions[course.name];
-    },
-
-    location: course => course.getLocation(),
+    return args.short ? course.name : fullCourseNames[course.name];
   },
+
+  date: (course, args, { utils: { format } }) => {
+    const { start, end, language } = args;
+    const { startDate, endDate } = course;
+
+    // full date range in language format if neither start nor end are passed
+    if ([start, end].every(arg => arg === undefined)) {
+      return format.courseDateRange(startDate, endDate, language);
+    }
+
+    // if there are args give precedence to start
+    return start ? startDate.toUTCString() : endDate.toUTCString();
+  },
+
+  description: (course, _, context) => {
+    // TODO: reimplement as a db fetch for admin editing
+    const { courseDescriptions } = context.utils.constants;
+    return courseDescriptions[course.name];
+  },
+
+  location: course => course.getLocation(),
+
+  students: (course, args) => course.getStudents(args),
+
+  payments: (course, args) => course.getPayments(args),
+};
+
+const Student = {
+  courses: student => student.getCourses(),
+  payments: student => student.getPayments(),
+};
+
+const Payment = {
+  course: payment => payment.getCourse(),
+  student: payment => payment.getStudent(),
+};
+
+module.exports = {
+  Course,
+  Student,
+  Payment,
 };
