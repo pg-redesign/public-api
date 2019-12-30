dc1fea27628f15fad7be2aa13094a406548ca65c
Vamp <vampiirecodes@gmail.com>
2019-12-30 01:41:29 -0600

    services/email: ref build credit link util to use JWT payload
    - ref tests and related usages

--- src/services/email/emailService.js
+++ src/services/email/emailService.js
@@ -10,7 +10,7 @@ module.exports = emailClient => ({
       .sendMail({
         to: student.email,
         from: constants.accounts.billing,
-        html: await renderCourseInvoice(course, student),
+        html: await renderCourseInvoice(course, student, context),
         subject: "Princeton Groundwater billing invoice",
       })
       .catch(error =>
