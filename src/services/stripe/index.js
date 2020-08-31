7003a6008e494667ef3097f24caaac791aebd687
the-vampiire <vampiirecodes@gmail.com>
2020-08-30 19:10:19 -0500

    stripe service: rename env var for clarity

--- src/services/stripe/index.js
+++ src/services/stripe/index.js
@@ -1,5 +1,5 @@
 const Stripe = require("stripe");
 
-const stripeClient = new Stripe(process.env.STRIPE_API_KEY);
+const stripeClient = new Stripe(process.env.STRIPE_PRIVATE_KEY);
 
 module.exports = require("./stripeService")(stripeClient);
