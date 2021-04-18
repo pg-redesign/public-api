ac29b3ac70a40b252b69d93bad35323fe3b7d79a
the-vampiire <vampiirecodes@gmail.com>
2021-04-18 01:05:29 -0500

    add createCourse form schema

--- src/schemas/forms/index.js
+++ src/schemas/forms/index.js
@@ -1,5 +1,6 @@
 const enums = require("../enums");
 const mailingList = require("./mailingList");
+const createCourse = require("./createCourse");
 const registration = require("./registration");
 const stripePayment = require("./stripePayment");
 
@@ -7,5 +8,6 @@ module.exports = {
   // ensures naming conistency across codebase
   [enums.FormTypes.mailingList]: mailingList,
   [enums.FormTypes.registration]: registration,
+  [enums.FormTypes.createCourse]: createCourse,
   [enums.FormTypes.stripePayment]: stripePayment,
 };
